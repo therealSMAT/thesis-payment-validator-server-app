9a44e38fd46a8f31dc2c3808583922b5137d70ff
Tosin Soremekun <t.soremekun@adcash.com>
2021-06-20 00:54:19 +0300

    Setup qr code endpoint

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.json({ status: true, message: 'How did you get here?' });
+});
+
+app.post('/qr', (req, res) => {
+    res.json({ status: true, data: req.body });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
