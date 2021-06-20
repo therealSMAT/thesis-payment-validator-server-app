fa8ce64243051d4632d2732740a7d1392da4c679
Tosin Soremekun <t.soremekun@adcash.com>
2021-06-20 13:22:10 +0300

    Work in Progress

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.post('/qr', (req, res) => {
     res.json({ status: true, data: req.body });
 });
 
+app.post('/test', (req, res) => {
+    res.json({ data: { tosin: 'Yes' } });
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
